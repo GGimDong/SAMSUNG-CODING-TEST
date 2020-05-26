b3ec1425e2bbdd3ba3764397eba15911f9d7dfc3
동은 김 <boramatlulu@gmail.com>
2020-05-26 14:07:27 +0900

    13460 도전 중

--- Samsung/Samsung/13460_구슬_탈출_2.cpp
+++ Samsung/Samsung/13460_구슬_탈출_2.cpp
@@ -2,174 +2,208 @@
 #include <cstdio>
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 
 using namespace std;
+
+struct pos {
+	int x;
+	int y;
+};
+
+pos init_R, init_B, hole;
 int N, M;
-char board[10][10];
-char temp_board[10][10];
+int min_result = -1;
 
-vector<int> v; // 1 2 3 4 상 하 좌 우
-int min_value = -1;
+/*	Board의 구성요소
+	0: 빈 곳
+	1: 벽
+	2: 구멍
+	3: Red
+	4: Blue
+*/
+int board[10][10];
 
-void print_board() {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++)
-			cout << temp_board[i][j];
-		cout << endl;
-	}
-	return;
-}
+// pos의 값들을 저장하기
+pos R[10][4];
+pos B[10][4];
 
-void copy_board() {
-	for (int i = 0; i < N; i++) 
-		for (int j = 0; j < M; j++)
-			temp_board[i][j] = board[i][j];
-	return;
-}
+/*지워*/void print_board() { for (int i = 0; i < N; i++) { for (int j = 0; j < M; j++) if (board[i][j] == 1)cout << "#"; else if (board[i][j] == 0) cout << " " ; else if (board[i][j] == 2) cout << "O"; else cout << board[i][j]; cout << endl; } }
 
-void move(int op) {
-	int t_i, t_j;
-
-	if(op ==1 || op == 3)
-		for (int i = 0; i < N; i++)
-			for (int j = 0; j < M; j++) {
-				if (temp_board[i][j] == 'R' || temp_board[i][j] == 'B') {
-					t_i = i;
-					t_j = j;
-					if (op == 1) {
-						while (temp_board[t_i - 1][t_j] == '.' || temp_board[t_i - 1][t_j] == 'O') {
-							if (temp_board[t_i - 1][t_j] == 'O') {
-								temp_board[t_i][t_j] = '.';
-								break;
-							}
-								temp_board[t_i - 1][t_j] = temp_board[t_i][t_j];
-								temp_board[t_i][t_j] = '.';
-								t_i--;
-						}
-					}
-					if (op == 3) {
-						while (temp_board[t_i][t_j - 1] == '.' || temp_board[t_i][t_j - 1] == 'O') {
-							if (temp_board[t_i][t_j - 1] == 'O') {
-								temp_board[t_i][t_j] = '.';
-								break;
-							}
-							temp_board[t_i][t_j - 1] = temp_board[t_i][t_j];
-							temp_board[t_i][t_j] = '.';
-							t_j--;
-						}
-					}
-				}
-			}
-	else if(op ==2 || op ==4)
-		for (int i = N-1; i >= 0; i--)
-			for (int j = M - 1; j >= 0; j--) {
-				if (temp_board[i][j] == 'R' || temp_board[i][j] == 'B') {
-					t_i = i;
-					t_j = j;
-					if (op == 2) {
-						while (temp_board[t_i + 1][t_j] == '.' || temp_board[t_i + 1][t_j] == 'O') {
-							if (temp_board[t_i + 1][t_j] == 'O') {
-								temp_board[t_i][t_j] = '.';
-								break;
-							}
-							temp_board[t_i + 1][t_j] = temp_board[t_i][t_j];
-							temp_board[t_i][t_j] = '.';
-							t_i++;
-						}
-					}
-					else if (op == 4){
-						while (temp_board[t_i][t_j + 1] == '.' || temp_board[t_i][t_j + 1] == 'O') {
-							if (temp_board[t_i][t_j + 1] == 'O') {
-								temp_board[t_i][t_j] = '.';
-								break;
-							}
-							temp_board[t_i][t_j + 1] = temp_board[t_i][t_j];
-							temp_board[t_i][t_j] = '.';
-							t_j++;
-						}
-					}
-				}
-			}
-	return;
-}
-
-int do_operation(int op) {
-	// temp_board 의 값들을 op 에 맞게 동작.
-	
-	move(op);
-	// 파란 구슬이 존재하는지 검사
-	int is_blue = -1;
-	int is_red = -1;
-
+/* 기존의 R, B를 지우고 새로운 R, B를 보드에 넣음 */
+void set_R_B(pos R, pos B) {
 	for (int i = 0; i < N; i++)
-		for (int j = 0; j < M; j++) {
-			if (temp_board[i][j] == 'B') is_blue = 1;
-			if (temp_board[i][j] == 'R') is_red = 1;
-		}
-	// 파란 구슬이 없으면 무조건 실패
-	if (is_blue == -1)
-		return -1;
+		for (int j = 0; j < M; j++)
+			if (board[i][j] == 3 || board[i][j] == 4)
+				board[i][j] = 0;
 
-	// 빨간 구슬이 없으면 이는 성공
-	if (is_red == -1)
-		return 1;
-
-	return 0;
-}
-
-void start_operation() {
-	copy_board();
-	for (int i = 0; i < v.size(); i++) {
-		int result = do_operation(v[i]);
-		if (result == 1) {
-			// 빨간 구슬만 쏙 나온 경우
-			int temp_min_value = i + 1;
-
-			if(min_value > temp_min_value || min_value == -1)
-				min_value = temp_min_value;
-			break;
-		}
-		else if (result == -1) // 어떻게든 파란 구슬도 쏙 나온 경우
-			break;
-	}
-}
-
-void operation_order() {
-	if (v.size() == 10) {
-		start_operation();
-		return;
-	}
-
-	v.push_back(1);
-	operation_order();
-	v.pop_back();
-
-	v.push_back(2);
-	operation_order();
-	v.pop_back();
-
-	v.push_back(3);
-	operation_order();
-	v.pop_back();
-
-	v.push_back(4);
-	operation_order();
-	v.pop_back();
+	board[R.x][R.y] = 3;
+	board[B.x][B.y] = 4;
 
 	return;
+}
+
+// 그 이전 값의 R과 B로 update
+void set_last_R_B() {
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			if (board[i][j] == 3 || board[i][j] == 4)
+				board[i][j] = 0;
+
+	board[init_R.x][init_R.y] = 3;
+	board[init_B.x][init_B.y] = 4;
+
+	return;
+}
+
+/*	1, 2의 경우 상단 위에서 돌리고 3, 4는 하단 아래에서 돌림.
+	>> op의 명령 종류
+	1: 상 
+	2: 좌 
+	3: 하 
+	4: 우
+	>> n은 타임에 대한 idx로 9가 max
+*/
+void move(int op) {
+	cout << "Op: " << op << endl;
+	int temp_i, temp_j;
+	if (op == 1 || op == 2) {
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < M; j++)
+				if (board[i][j] == 3 || board[i][j] == 4) {
+					temp_i = i;
+					temp_j = j;
+					if (op == 1) {
+						/*지워*/cout << "Up" << endl;
+						while (board[temp_i - 1][temp_j] == 2 || board[temp_i - 1][temp_j] == 0) {
+							if (board[temp_i - 1][temp_j] == 0) {
+								board[temp_i - 1][temp_j] = board[temp_i][temp_j];
+								board[temp_i][temp_j] = 0;
+							}
+							else if (board[temp_i - 1][temp_j] == 2) {
+								board[temp_i][temp_j] = 0;
+								break;
+							}
+							temp_i--;
+							//temp_j--;
+						}
+					}
+					else {
+						/*지워*/cout << "Left" << endl;
+						while (board[temp_i][temp_j - 1] == 2 || board[temp_i][temp_j - 1] == 0) {
+							if (board[temp_i][temp_j - 1] == 0) {
+								board[temp_i][temp_j - 1] = board[temp_i][temp_j];
+								board[temp_i][temp_j] = 0;
+							}
+							else if (board[temp_i][temp_j - 1] == 2) {
+								board[temp_i][temp_j] = 0;
+								break;
+							}
+							//temp_i--;
+							temp_j--;
+						}
+					}
+				}
+	}
+	else {
+		for (int i = N - 1; i >= 0; i--)
+			for (int j = M - 1; j >= 0; j--)
+				if (board[i][j] == 3 || board[i][j] == 4) {
+					temp_i = i;
+					temp_j = j;
+					if (op == 3) {
+						/*지워*/cout << "Down" << endl;
+						while (board[temp_i + 1][temp_j] == 2 || board[temp_i + 1][temp_j] == 0) {
+							if (board[temp_i + 1][temp_j] == 0) {
+								board[temp_i + 1][temp_j] = board[temp_i][temp_j];
+								board[temp_i][temp_j] = 0;
+							}
+							else if (board[temp_i + 1][temp_j] == 2) {
+								board[temp_i][temp_j] = 0;
+								break;
+							}
+							temp_i++;
+							//temp_j--;
+						}
+					}
+					else {
+						/*지워*/cout << "Right" << endl;
+						while (board[temp_i][temp_j + 1] == 2 || board[temp_i][temp_j + 1] == 0) {
+							if (board[temp_i][temp_j + 1] == 0) {
+								board[temp_i][temp_j + 1] = board[temp_i][temp_j];
+								board[temp_i][temp_j] = 0;
+							}
+							else if (board[temp_i][temp_j + 1] == 2) {
+								board[temp_i][temp_j] = 0;
+								break;
+							}
+							//temp_i--;
+							temp_j++;
+						}
+					}
+				}
+	}
+
+	/*지워*/ print_board(); system("pause");
+
+	return;
+}
+
+
+void BFS(pos R, pos B, int n) {
+	if(n == 10)
+		return;
+
+	move(3);
+	set_last_R_B();
+
+	move(1);
+	set_last_R_B();
+
+	move(2);
+	set_last_R_B();
+
+	move(4);
+	set_last_R_B();
+
 }
 
 int main() {
 	ifstream input;
 	input.open("input/input.txt");
-	
+	char temp;
+
+	// initialize
 	input >> N >> M;
 	for (int i = 0; i < N; i++)
-		for (int j = 0; j < M; j++) 
-			input >> board[i][j];
+		for (int j = 0; j < M; j++){
+			input >> temp;
+			if (temp == '#') 
+				board[i][j] = 1;
+			else if(temp == '.')
+				board[i][j] = 0;
+			else if (temp == 'R') {
+				board[i][j] = 3;
+				init_R.x = i;
+				init_R.y = j;
+			}
+			else if (temp == 'B') {
+				board[i][j] = 4;
+				init_B.x = i;
+				init_B.y = j;
+			}
+			else if (temp == 'O') {
+				board[i][j] = 2;
+				hole.x = i;
+				hole.y = j;
+			}
+		}
 	
-	operation_order();
+	/*지워*/cout << "INIT: " << endl; print_board();
 	
-	cout << min_value;
+
+	BFS(init_R, init_B, 0);
+
+	cout << min_result;
 	return 0;
 }
