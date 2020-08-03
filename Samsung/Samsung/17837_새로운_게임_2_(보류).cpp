259fd0b335c9c47ea705ad183f5a7145708964e3
동은 김 <boramatlulu@gmail.com>
2020-08-03 23:12:43 +0900

    ;

--- Samsung/Samsung/17837_새로운_게임_2_(보류).cpp
+++ Samsung/Samsung/17837_새로운_게임_2_(보류).cpp
@@ -1,90 +1,124 @@
 #include <iostream>
 
 using namespace std;
-						// 1우	  2좌	  3상	  4하
-int dir[5][2] = { {0, 0}, {0,1}, {0,-1}, {-1,0}, {1,0} };
-int map[12][12][11];	// 0에는 색깔 1~10은 체스말 존재의 순서
-int pieceDir[11];		// 1~10까지 체스말 방향
-int pieceInfo[11][2];
-int N, K, a, b, c;
 
-void move(int idx) {
-	int x = pieceInfo[idx][0], y = pieceInfo[idx][1];
-	int color = map[x][y][0], level;
-
-	for (int k = 1; k <= 10; k++) if (map[x][y][k] == idx) level = k;
-
-	/*DEBUG*/cout << idx << "(" << x << "," << y << ") " << level << endl;
-	int nx = x + dir[pieceDir[idx]][0], ny = y + dir[pieceDir[idx]][1];
-
-	if (nx < 0 || ny < 0 || nx >= N || ny >= ny || color == 2) { // 바깥쪽, 파란색
-		int d = pieceDir[idx];
-		if (d == 1) pieceDir[idx] = 2;
-		else if (d == 2)pieceDir[idx] = 1;
-		else if (d == 3)pieceDir[idx] = 4;
-		else if (d == 4)pieceDir[idx] = 3;
-		nx = x + dir[pieceDir[idx]][0], ny = y + dir[pieceDir[idx]][1];
-		if (map[nx][ny][0] == 2) return;	// 뒤돌아서서 봤드만 못가는 길이면 야는 그냥 고정
+struct Piece {
+	int x, y, d;
+	void changeDir() {
+		if (d == 1) d = 2;
+		else if (d == 2) d = 1;
+		else if (d == 3) d = 4;
+		else if (d == 4) d = 3;
 	}
-	
-	// 움직임!
+}pieces[11];
+
+int dir[5][2] = { {0,0}, {0, 1}, {0,-1}, {-1,0}, {1,0} };
+int map[14][14], order[14][14][10];
+int N, K;
+
+/*디버깅*/void printOrder() {
+	for (int i = 1; i <= N; i++)
+		for (int j = 1; j <= N; j++)
+			if (order[i][j][0] != 0) {
+				cout << "(" << i << "," << j << "): ";
+				for (int k = 0; k < K; k++) {
+					if (order[i][j][k] == 0) break;
+					cout << order[i][j][k] << "("<< pieces[order[i][j][k]].d <<") ";
+				}
+				cout << endl;
+			}
+}
+
+void moveWhite(int x, int y, int nx, int ny, int level) {
 	int s;
-	for (int i = 1; i <= 10; i++) 
-		if (map[x][y][i] == idx) {
+	for (int i = 0; i < 10; i++) 
+		if (order[nx][ny][i] == 0) {
 			s = i;
 			break;
 		}
+	
+	for (int i = level; i < 10; i++) {
+		int idx = order[x][y][i];
+		if (idx == 0) break;
+		pieces[idx].x = nx, pieces[idx].y = ny;
 
-	for (int i = 1; i <= 10; i++) {
-		if (map[nx][ny][i] != 0) continue; 
-		map[nx][ny][i] = map[x][y][s];
-		map[x][y][s++] = 0;
+		order[nx][ny][s++] = idx;
+		order[x][y][i] = 0;
+
 	}
-
-
-	if (color == 1){ // 빨간색일 경우, 위아래 반전
-
-	}
-		
+	//printOrder();
 }
 
-void print_board() {
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			for (int k = 1; k < 11; k++)
-				if (map[i][j][k] != 0)
-					cout <<"("<< i << "," << j << ")_"<< k <<": "<< map[i][j][k] << endl;
-	cout << endl;
+// 뒤로 돌아서 가세요
+void moveBlue(int idx, int level) {
+	pieces[idx].changeDir();
+	int x = pieces[idx].x, y = pieces[idx].y, d = pieces[idx].d;
+	int nx = x + dir[d][0], ny = y + dir[d][1];
+
+	// 뒤돌아도 못가면 바로 끝
+	if (nx == 0 || ny == 0 || nx == N + 1 || ny == N + 1) return;
+	else if (map[nx][ny] == 2) return;
+
+	moveWhite(x, y, nx, ny, level);
+}
+
+void move(int idx) {
+	int x = pieces[idx].x, y = pieces[idx].y, d = pieces[idx].d;
+	int nx = x + dir[d][0], ny = y + dir[d][1];
+	int level;
+	for (int i = 0; i < K; i++) if (order[x][y][i] == idx) level = i;
+
+	// 못 가는 곳이거나 파란곳으로 향할 땐 동일하게 처리
+	if (nx == 0 || ny == 0 || nx == N + 1 || ny == N + 1) moveBlue(idx, level);
+	else if (map[nx][ny] == 2) moveBlue(idx, level);
+	else {
+		// 움직임 이동
+		moveWhite(x, y, nx, ny, level);
+		// 빨간색이면 순서 바꾸기만 추가
+		if (map[nx][ny] == 1) {
+			int temp[10] = { 0, };
+			for (int i = 0; i < K; i++) if (order[nx][ny][i] == idx) level = i;
+			int t_i = 0;
+			for (int i = 9; i >= level; i--) {
+				if (order[nx][ny][i] != 0) temp[t_i++] = order[nx][ny][i];
+			}
+
+			//cout << "DEBUG ";
+			for (int i = 0; i < 10; i++) {
+				if (temp[i] == 0) break;
+				//cout << temp[i] << " ";
+				order[nx][ny][level + i] = temp[i];
+			}//cout << endl;
+		}
+	}
 }
 
 int main() {
 	cin >> N >> K;
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			cin >> map[i][j][0];
+	for (int i = 1; i <= N; i++)
+		for (int j = 1; j <= N; j++) 
+			cin >> map[i][j];
 
 	for (int i = 1; i <= K; i++) {
-		cin >> a >> b >> pieceDir[i];
-		a--, b--, map[a][b][1] = i;
-		pieceInfo[i][0] = a, pieceInfo[i][1] = b;
+		cin >> pieces[i].x >> pieces[i].y >> pieces[i].d;
+		order[pieces[i].x][pieces[i].y][0] = i;
 	}
+	
 
-	cout << "Start" << endl; print_board();
 	for (int time = 1; time <= 1000; time++) {
-		for (int i = 1; i <= K; i++) move(i);
-		
-		// 4개가 쌓일 경우 종료
-		for (int i = 0; i < N; i++) 
-			for (int j = 0; j < N; j++) 
-				if (map[i][j][4] != 0) {
-					cout << time;
-					return 0;
-				}
-
-		cout << "Time: " << time << endl; print_board();
-		system("pause");
+		//cout << "Time: " << time << endl;
+		for (int i = 1; i <= K; i++) {
+			//cout << "Idx: " << i << endl;
+			move(i);
+			//system("pause");
+			for (int j = 1; j <= N; j++)
+				for (int k = 1; k <= N; k++)
+					if (order[j][k][3] != 0) {
+						cout << time;
+						return 0;
+					}
 	}
-
+	}
 	cout << -1;
 	return 0;
 }
